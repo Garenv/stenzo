a28364a2fbdb6056c3ca4aa0acac2e1b4dfc05e5
Garen Vartanian <garenvartanian@Garens-Air.fios-router.home>
2018-12-08 08:27:13 -0500

    another commit

---
+++ src/Home/Home.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Link } from 'react-router-dom';
+import Next from "./Next";
+
+class Home extends Component {
+    constructor(props) {
+        super(props);
+        this.goNext = this.goNext.bind(this);
+    }
+
+    goNext = (e) => {
+        alert("hey");
+    };
+
+    render() {
+        return (
+            <div>
+                <form action="http://localhost/demo_react/api/demo.php" method={"POST"} encType="multipart/form-data">
+
+                    <div className="form-group">
+                        <label htmlFor="username">Email</label>
+                        <input className="form-control" type="text" name="username"/>
+                    </div>
+
+                    <div className="form-group">
+                        <label htmlFor="password">Password</label>
+                        <input className="form-control" type="password" name="password"/>
+                    </div>
+
+                    <Router>
+                        <Link to={Next}>
+                            <button className="btn btn-primary" onSubmit={(e) => this.goNext(e)} value="Login" name={"submit"}>Submit</button>
+                        </Link>
+                    </Router>
+
+                </form>
+            </div>
+        );
+    }
+}
+
+export default Home;
---
+++ src/Home/Next.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Next extends Component {
+    render() {
+        return(
+            <h1>made it</h1>
+        );
+    }
+}
+
+export default Next;
